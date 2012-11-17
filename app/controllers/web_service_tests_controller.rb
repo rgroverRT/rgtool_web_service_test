cf3cb83722a35a9b83ed1c4ce2716dc117acf833
Russ Grover <rgrover@redtailtechnology.com>
2012-11-16 16:56:17 -0800

    added some error handling, extra error banners;  hacky!

--- app/controllers/web_service_tests_controller.rb
+++ app/controllers/web_service_tests_controller.rb
@@ -3,6 +3,9 @@ require 'net/http'
 class WebServiceTestsController < ApplicationController
 
     DEFAULT_SERVER = "http://localhost:3000"
+    DEFAULT_RESOURCE = '/subscriptions'     #/oldest'
+    DEFAULT_METHOD =  "-- Select a method to test --"
+    METHOD_LIST = [DEFAULT_METHOD, "GET", "PUT"]
 
     def new
         logger.debug("'new' method called in #{self.controller_name}")
@@ -10,13 +13,11 @@ class WebServiceTestsController < ApplicationController
         @web_service_test = WebServiceTest.new
 
         @web_service_test.name = "WST-" + DateTime::now.strftime("%I:%M:%S %p")
-
-        @web_service_test.test_method = "-- Select a method to test --"
+        @web_service_test.test_method =
         @web_service_test.server = DEFAULT_SERVER
-        @web_service_test.resource = '/subscriptions/oldest'
+        @web_service_test.resource = DEFAULT_RESOURCE
 
-        @test_method_list = ["-- Select a method to test --", "GET", "PUT"]
-
+        @test_method_list = METHOD_LIST #[DEFAULT_METHOD, "GET", "PUT"]
         @test_response = session[:test_response] ||= {response_text: "will go here ..."}
     end
 
@@ -26,28 +27,43 @@ class WebServiceTestsController < ApplicationController
         @web_service_test = WebServiceTest.new(params[:web_service_test])
 
         if @web_service_test.valid?
-            # HERE IT IS, the CALL!!
-            test_response = make_request(@web_service_test)
+
+            # HERE IT IS, the REMOTE CALL!
+            begin
+                test_response = make_request(@web_service_test)
+            rescue => ex
+                form_init
+                @connection_error = "#{ex.class}: #{ex.message}"
+                render 'new'
+                return
+            end
 
             # Pass it across the redirect - TODO:  figure if there's another way
             session[:test_response] = test_response
             #{response_text: DateTime::now.seconds_since_midnight.to_s + ":  hard-coded response;  would send, recieve, and update this text ..."}
 
-            @web_service_test.status = test_response.status
+            # Add some explanation for bad status codes
+            if test_response.status != 200
+                form_init
+                @soft_connection_error = test_response.status.to_s + ":  " + @error_message
+                render 'new'
+                return
+            else
+                @web_service_test.status = test_response.status
 
-            if params[:db_save] == "on"
-                logger.debug("saving this WST:  " + @web_service_test.to_s)
-                # NOTE:  if saved, form wants to "PUT" next request, so need "UPDATE" method, yada yada
-                @web_service_test.save
+                if params[:db_save] == "on"
+                    logger.debug("saving this WST:  " + @web_service_test.to_s)
+                    # NOTE:  if saved, form wants to "PUT" next request, so need "UPDATE" method, yada yada
+                    @web_service_test.save
+                end
+
+                # wish we could ...
+                # render 'new'
+                # but routing problems ... TODO - full Rails way
+                redirect_to new_web_service_test_path()
             end
-
-            redirect_to new_web_service_test_path()
         else
-            #@web_service_test.name = "WST-" + DateTime::now.strftime("%I:%M:%S %p")
-            #@web_service_test.test_method = "-- Select a method to test --"
-            @test_method_list = ["-- Select a method to test --", "GET", "PUT"]
-            @test_response = session[:test_response] ||= {response_text: "will go here ..."}
-
+            form_init
             # If we did this direct render call, we would have to set up all the default form values;  BUT!
             # We can't do this, as it forces a call to the 'update' method in the controller *iff* the database was written
             render 'new'
@@ -55,6 +71,13 @@ class WebServiceTestsController < ApplicationController
     end
 
     private
+        def form_init
+            @test_method_list = METHOD_LIST #[DEFAULT_METHOD, "GET", "PUT"]
+            @test_response = session[:test_response] ||= {response_text: "will go here ..."}
+            @connection_error = nil
+            @soft_connection_error = nil
+        end
+
         # TODO: try setting some header info for the request type instead of just hammering the file suffix to the mime type
         #
         def make_request(in_web_service_test)
@@ -103,7 +126,9 @@ class WebServiceTestsController < ApplicationController
             test_response.response_text = in_web_service_test.name + ":\n" +  raw_response.to_s #for now;  TODO clean this up, parse, extract, etc.
             #DateTime::now.seconds_since_midnight.to_s + ":  "
 
+            #hack return extra stuff;  many other success code, so there's got to be a Rails way for this
+            @error_message = res.message
+
             return test_response
         end
-
 end
